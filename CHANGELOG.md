c62ff09702439804a41a4017fbb32803e7797d53
Lukas Hirt <lukashirt@mail.com>
2020-08-17 14:18:56 +0200

    Use ocis-boilerplate

---
+++ CHANGELOG.md
@@ -0,0 +1,25 @@
+# Changelog for unreleased
+
+The following sections list the changes for unreleased.
+
+## Summary
+
+ * Chg #1: Initial release of basic version
+ * Chg #11: Upgrade micro libraries
+
+## Details
+
+ * Change #1: Initial release of basic version
+
+   Just prepared an initial basic version to serve OCS for the ownCloud Infinite Scale project. It
+   just provides a minimal viable product to demonstrate the microservice pattern.
+
+   https://github.com/owncloud/ocis-ocs/issues/1
+
+ * Change #11: Upgrade micro libraries
+
+   Updated the micro and ocis-pkg libraries to version 2.
+
+   https://github.com/owncloud/ocis-ocs/issues/11
+
+
---
+++ pkg/flagset/flagset.go
@@ -0,0 +1,138 @@
+package flagset
+
+import (
+	"github.com/micro/cli/v2"
+	"github.com/owncloud/ocis-markdown-editor/pkg/config"
+)
+
+// RootWithConfig applies cfg to the root flagset
+func RootWithConfig(cfg *config.Config) []cli.Flag {
+	return []cli.Flag{
+		&cli.StringFlag{
+			Name:        "config-file",
+			Value:       "",
+			Usage:       "Path to config file",
+			EnvVars:     []string{"MARKDOWN-EDITOR_CONFIG_FILE"},
+			Destination: &cfg.File,
+		},
+		&cli.StringFlag{
+			Name:        "log-level",
+			Value:       "info",
+			Usage:       "Set logging level",
+			EnvVars:     []string{"MARKDOWN-EDITOR_LOG_LEVEL"},
+			Destination: &cfg.Log.Level,
+		},
+		&cli.BoolFlag{
+			Name:        "log-pretty",
+			Usage:       "Enable pretty logging",
+			EnvVars:     []string{"MARKDOWN-EDITOR_LOG_PRETTY"},
+			Destination: &cfg.Log.Pretty,
+		},
+		&cli.BoolFlag{
+			Name:        "log-color",
+			Usage:       "Enable colored logging",
+			EnvVars:     []string{"MARKDOWN-EDITOR_LOG_COLOR"},
+			Destination: &cfg.Log.Color,
+		},
+	}
+}
+
+// HealthWithConfig applies cfg to the root flagset
+func HealthWithConfig(cfg *config.Config) []cli.Flag {
+	return []cli.Flag{
+		&cli.StringFlag{
+			Name:        "debug-addr",
+			Value:       "0.0.0.0:9114",
+			Usage:       "Address to debug endpoint",
+			EnvVars:     []string{"MARKDOWN-EDITOR_DEBUG_ADDR"},
+			Destination: &cfg.Debug.Addr,
+		},
+	}
+}
+
+// ServerWithConfig applies cfg to the root flagset
+func ServerWithConfig(cfg *config.Config) []cli.Flag {
+	return []cli.Flag{
+		&cli.BoolFlag{
+			Name:        "tracing-enabled",
+			Usage:       "Enable sending traces",
+			EnvVars:     []string{"MARKDOWN-EDITOR_TRACING_ENABLED"},
+			Destination: &cfg.Tracing.Enabled,
+		},
+		&cli.StringFlag{
+			Name:        "tracing-type",
+			Value:       "jaeger",
+			Usage:       "Tracing backend type",
+			EnvVars:     []string{"MARKDOWN-EDITOR_TRACING_TYPE"},
+			Destination: &cfg.Tracing.Type,
+		},
+		&cli.StringFlag{
+			Name:        "tracing-endpoint",
+			Value:       "",
+			Usage:       "Endpoint for the agent",
+			EnvVars:     []string{"MARKDOWN-EDITOR_TRACING_ENDPOINT"},
+			Destination: &cfg.Tracing.Endpoint,
+		},
+		&cli.StringFlag{
+			Name:        "tracing-collector",
+			Value:       "",
+			Usage:       "Endpoint for the collector",
+			EnvVars:     []string{"MARKDOWN-EDITOR_TRACING_COLLECTOR"},
+			Destination: &cfg.Tracing.Collector,
+		},
+		&cli.StringFlag{
+			Name:        "tracing-service",
+			Value:       "ocs",
+			Usage:       "Service name for tracing",
+			EnvVars:     []string{"MARKDOWN-EDITOR_TRACING_SERVICE"},
+			Destination: &cfg.Tracing.Service,
+		},
+		&cli.StringFlag{
+			Name:        "debug-addr",
+			Value:       "0.0.0.0:9114",
+			Usage:       "Address to bind debug server",
+			EnvVars:     []string{"MARKDOWN-EDITOR_DEBUG_ADDR"},
+			Destination: &cfg.Debug.Addr,
+		},
+		&cli.StringFlag{
+			Name:        "debug-token",
+			Value:       "",
+			Usage:       "Token to grant metrics access",
+			EnvVars:     []string{"MARKDOWN-EDITOR_DEBUG_TOKEN"},
+			Destination: &cfg.Debug.Token,
+		},
+		&cli.BoolFlag{
+			Name:        "debug-pprof",
+			Usage:       "Enable pprof debugging",
+			EnvVars:     []string{"MARKDOWN-EDITOR_DEBUG_PPROF"},
+			Destination: &cfg.Debug.Pprof,
+		},
+		&cli.BoolFlag{
+			Name:        "debug-zpages",
+			Usage:       "Enable zpages debugging",
+			EnvVars:     []string{"MARKDOWN-EDITOR_DEBUG_ZPAGES"},
+			Destination: &cfg.Debug.Zpages,
+		},
+		&cli.StringFlag{
+			Name:        "http-addr",
+			Value:       "0.0.0.0:9110",
+			Usage:       "Address to bind http server",
+			EnvVars:     []string{"MARKDOWN-EDITOR_HTTP_ADDR"},
+			Destination: &cfg.HTTP.Addr,
+		},
+		&cli.StringFlag{
+			Name:        "http-namespace",
+			Value:       "com.owncloud.web",
+			Usage:       "Set the base namespace for the http namespace",
+			EnvVars:     []string{"MARKDOWN-EDITOR_NAMESPACE"},
+			Destination: &cfg.HTTP.Namespace,
+		},
+		&cli.StringFlag{
+			Name:        "http-root",
+			Value:       "/",
+			Usage:       "Root path of http server",
+			EnvVars:     []string{"MARKDOWN-EDITOR_HTTP_ROOT"},
+			Destination: &cfg.HTTP.Root,
+		},
+	}
+}
